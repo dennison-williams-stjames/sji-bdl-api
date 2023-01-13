dfa3c0605b3bc651c9512b74847e377da6ba187f
Dennison Williams <dennison.williams@stjamesinfirmary.org>
2023-01-12 16:06:37 -0800

    Creating a port name unique from the one used in wait-for.sh

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = require('./app').app;
-const port = process.env.PORT || 3001;
+const port = process.env.API_PORT || 3001;
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
