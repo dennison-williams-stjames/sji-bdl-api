e6be97baa4604f92384d196f7618a421e01b64a6
Dennison Williams <dennison.williams@stjamesinfirmary.org>
2023-01-23 12:27:18 -0800

    Adding morgan logging to the application stack

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const app = require('./app').app;
 const port = process.env.API_PORT || 3001;
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
